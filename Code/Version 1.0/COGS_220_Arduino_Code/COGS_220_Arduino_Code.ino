a9b1cb566066a37ffd26d6726948feba35f2fc92
Wenxuan Guo <wguo@vassar.edu>
2018-02-06 18:19:55 -0500

    QRE
    Code for lIne sensor

--- Code/Version 1.0/COGS_220_Arduino_Code/COGS_220_Arduino_Code.ino
+++ Code/Version 1.0/COGS_220_Arduino_Code/COGS_220_Arduino_Code.ino
@@ -31,7 +31,7 @@ void setup() {
      By the way, auto-indent is Ctrl-t.
   */
   //initialize camera
-  camera.init();
+  Pixy.init();
 
   //Set the initial servo positions:
   LeftWheel.writeMicroseconds(1500);
@@ -85,23 +85,18 @@ void loop() {
 
 //Drives towards the closest red block
 void dtr() {
-
-  uint16_t blocks;
-  int targetIndex;
-  float targetX;
-  float direct;
   
   //fetch all color blocks for further processing
   blocks = camera.getBlocks();
 
   //the largest block will be the target
-  float sizes[sizeof(blocks)];
+  float sizes[];
 
   //use x-coordinate to guide
-  float x[sizeof(blocks)];
+  float x[];
 
   //get all red blocks
-  for (int i = 0; i < sizeof(blocks); i++) {
+  for (int i = 0; i < sizeOf(blocks); i++) {
     if (camera.blocks[i].signature == 1) {
       sizes[i] = camera.blocks[i].width * camera.blocks[i].height;
       x[i] = camera.blocks[i].x;
@@ -124,23 +119,18 @@ void dtr() {
 
 //Drives towards the closest green block
 void dtg() {
-
-  uint16_t blocks;
-  int targetIndex;
-  float targetX;
-  float direct;
   
   //fetch all color blocks for further processing
   blocks = camera.getBlocks();
 
   //the largest block will be the target
-  float sizes[sizeof(blocks)];
+  float sizes[];
 
   //use x-coordinate to guide
-  float x[sizeof(blocks)];
+  float x[];
 
   //get all green blocks
-  for (int i = 0; i < sizeof(blocks); i++) {
+  for (int i = 0; i < sizeOf(blocks); i++) {
     if (camera.blocks[i].signature == 2) {
       sizes[i] = camera.blocks[i].width * camera.blocks[i].height;
       x[i] = camera.blocks[i].x;
@@ -163,23 +153,18 @@ void dtg() {
 
 //Drives towards the closest blue block
 void dtb() {
-
-  uint16_t blocks;
-  int targetIndex;
-  float targetX;
-  float direct;
   
   //fetch all color blocks for further processing
   blocks = camera.getBlocks();
 
   //the largest block will be the target
-  float sizes[sizeof(blocks)];
+  float sizes[];
 
   //use x-coordinate to guide
-  float x[sizeof(blocks)];
+  float x[];
 
   //get all blue blocks
-  for (int i = 0; i < sizeof(blocks); i++) {
+  for (int i = 0; i < sizeOf(blocks); i++) {
     if (camera.blocks[i].signature == 3) {
       sizes[i] = camera.blocks[i].width * camera.blocks[i].height;
       x[i] = camera.blocks[i].x;
@@ -202,23 +187,18 @@ void dtb() {
 
 //Drives towards the closest yellow block
 void dty() {
-
-  uint16_t blocks;
-  int targetIndex;
-  float targetX;
-  float direct;
   
   //fetch all color blocks for further processing
   blocks = camera.getBlocks();
 
   //the largest block will be the target
-  float sizes[sizeof(blocks)];
+  float sizes[];
 
   //use x-coordinate to guide
-  float x[sizeof(blocks)];
+  float x[];
 
   //get all yellow blocks
-  for (int i = 0; i < sizeof(blocks); i++) {
+  for (int i = 0; i < sizeOf(blocks); i++) {
     if (camera.blocks[i].signature == 4) {
       sizes[i] = camera.blocks[i].width * camera.blocks[i].height;
       x[i] = camera.blocks[i].x;
@@ -239,13 +219,11 @@ void dty() {
   }
 }
 
-/*
 void detectWhite() {
   if (lineSensor.digitalRead() == HIGH) {
     Drive(-50, -50, 1);
   }
 }
-*/
 
 void Drive(float ls, float rs, float d) {
   /*
@@ -258,7 +236,7 @@ void Drive(float ls, float rs, float d) {
 }
 
 //helper function used to get the minimum index from a list
-int getMax(float* array)
+int getMax(int* array)
 {
   int maxIndex = 0;
   int size = sizeof(array);
